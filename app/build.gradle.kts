c715214fe19c18153768c21d03338b53e2534885
WennyLi <lily51527@gmail.com>
2026-05-03 03:51:26 +0800

    Update: Configure release build type to use debug signing configuration in build.gradle.kts.

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -27,6 +27,7 @@ android {
 
     buildTypes {
         release {
+            signingConfig = signingConfigs.getByName("debug")
             optimization {
                 enable = false
             }
