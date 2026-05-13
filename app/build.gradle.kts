3d3f976f845bb99254d3f65821c237c58b8fae7d
WennyLi <lily51527@gmail.com>
2026-05-14 01:32:07 +0800

    chore(deps): add assertk and kotlin.test to test dependencies

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -71,6 +71,8 @@ dependencies {
     testImplementation(libs.mockk)
     testImplementation(libs.kotlinx.coroutines.test)
     testImplementation(libs.turbine)
+    testImplementation(libs.assertk)
+    testImplementation(kotlin("test"))
     androidTestImplementation(platform(libs.androidx.compose.bom))
     androidTestImplementation(libs.androidx.compose.ui.test.junit4)
     androidTestImplementation(libs.androidx.espresso.core)
